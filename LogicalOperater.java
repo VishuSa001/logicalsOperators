3e81e7a7a92a7a8dc403d9373dcda8b26286b4b6
VishuSa001 <114149406+VishuSa001@users.noreply.github.com>
2022-09-30 03:13:54 -0700

    Add files via upload

---
+++ LogicalOperater.java
@@ -0,0 +1,30 @@
+
+// public class LogicalOperater {
+//     public static void main(String[] args) {
+
+//     }
+// }
+
+import java.util.Scanner;
+
+public class LogicalOperater {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        System.out.println("this is the first number");
+        int num2 = sc.nextInt();
+        System.out.println("this is the Second  number");
+        int num3 = sc.nextInt();
+        System.out.println("this is the third number");
+
+        // use AND OPERATOR, condition and compare number together
+        if ((num1 < num2) || (num2 == num3)) {
+            int sum = num1 + num2 + num3;
+            System.out.println("the sum is : " + sum);
+
+        } else {
+            System.out.println("false condition");
+        }
+
+    }
+}
